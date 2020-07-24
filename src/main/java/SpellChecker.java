6f72afcaa4a96e8f8b5b330bb5c7003aa3f13abd
Makhya Wilridge <makhyaw@makhyas-air.myfiosgateway.com>
2020-07-23 22:19:07 -0400

    part 1 done

--- src/main/java/SpellChecker.java
+++ src/main/java/SpellChecker.java
@@ -1,2 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import java.util.Scanner;
+
 public class SpellChecker {
+    public HashSet<String> dictionary = new HashSet<String>(370104); //declares hashset variable name
+    public HashSet<String> misspelledWords;
+
+    public static String removeLowerCaseandPunc(String s){
+        String newStr; //for s in StrArray
+        //remove evil punctuation and convert the whole line to lowercase
+        newStr = s.toLowerCase().replaceAll("[^a-zA-Z\\s]", "").replaceAll("\\s+", " ");
+        return newStr;
+    }
+
+
+    public void initSet() throws FileNotFoundException, FileNotFoundException {
+        Scanner sc = new Scanner(new File("letter_from_gandhi.txt"));
+        while (sc.hasNextLine()) { //run thru file
+            String[] line = getLine(sc.nextLine()); //built in nextLine gets first line from  the file of the input scaner sc, passes to getline
+             misspelledWords = new HashSet<String>(Arrays.asList(line)); //putting values into hash set
+        }
+    }
+
+    public void dictionarySetUp() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("words_alpha.txt"));
+        while (sc.hasNextLine()) { //run thru file
+            dictionary.add(sc.nextLine().toLowerCase()); // adds each and every word into our set
+
+        }
+
+    }
+    public String[] getLine(String nextLine){
+        String [] line=nextLine.split(" "); //splits  first line into a string array
+        return line; //passes string array back to initSet String [] line
+    }
 }
--- src/test/java/SpellCheckerTest.java
+++ src/test/java/SpellCheckerTest.java
@@ -1,2 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.Scanner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
 public class SpellCheckerTest {
+    SpellChecker spellChecker;
+    @Before
+    public void setup() {
+        this.spellChecker = new SpellChecker();
+    }
+    @Test
+    public void readFileTest() throws FileNotFoundException {
+        //given
+        Scanner sc = new Scanner(new File("letter_from_gandhi.txt"));
+        String [] expectedLine= {"Dear","friend,"}; //a split array
+        //when action
+        String [] actualLine= spellChecker.getLine(sc.nextLine()); //calls getLine fx
+        //then 
+        Assert.assertEquals(expectedLine,actualLine);
+    }
+    @Test
+    public void correctSpellingTest(){
+    }
+    @Test
+    public void dictionarySetUpTest() throws FileNotFoundException {
+        //action
+        spellChecker.dictionarySetUp(); // void function the puts everything into our dictionary set
+        //then
+        Assert.assertTrue(spellChecker.dictionary.contains("a")); // test if a is in our set
+    }
+    @Test
+    public void testTreeSet(){
+        //should be alpha order 
+    }
 }
